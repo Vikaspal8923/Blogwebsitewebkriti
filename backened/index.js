892c2f1d1a2a0a42aca533ef5371d69e1994b3c8
Vikas_pal8923 <145531494+Vikaspal8923@users.noreply.github.com>
2024-09-29 14:26:16 +0530

    Update index.js

--- backened/index.js
+++ backened/index.js
@@ -42,7 +42,7 @@ app.use(
       cors({
                origin: [
                 'https://blogfusion123.vercel.app',
-                'http://127.0.0.1:5500/'
+                'http://127.0.0.1:5500'
                        
                        ],
                credentials:true
