d9f86b026e445a001b4b7f4ce0e815335c204f2c
vikaspal8923 <padhaanvikaspal1001@gmail.com>
2024-09-30 23:04:29 +0530

    c

--- backened/routes/sitemap.js
+++ backened/routes/sitemap.js
@@ -9,30 +9,30 @@ router.get('/sitemap.xml', async (req, res) => {
     try {
 
         console.log(" sitemap request recievd");
-        // Create a stream to write to
-        // console.log('Sitemap request received'); // Debug log
-        // const sitemapStream = new SitemapStream({ hostname: 'https://blogfusion123.vercel.app' });
+       // Create a stream to write to
+        console.log('Sitemap request received'); // Debug log
+        const sitemapStream = new SitemapStream({ hostname: 'https://blogfusion123.vercel.app' });
 
-        // // Fetch all blog posts from your database
-        // const blogPosts = await Blogpost.find(); // Ensure this returns all the necessary blog post URLs
+        // Fetch all blog posts from your database
+        const blogPosts = await Blogpost.find(); // Ensure this returns all the necessary blog post URLs
 
-        // // Add each blog post to the sitemap
-        // blogPosts.forEach((post) => {
-        //     sitemapStream.write({
-        //         url: `/previewblog.html?postid=${post._id}`, // Adjusting URL structure based on your blog's URL format
-        //         changefreq: 'daily',
-        //         priority: 0.7,
-        //         lastmod: post.updatedat // Assuming you have a timestamp for when the post was last updated
-        //     });
-        // });
+        // Add each blog post to the sitemap
+        blogPosts.forEach((post) => {
+            sitemapStream.write({
+                url: `/previewblog.html?postid=${post._id}`, // Adjusting URL structure based on your blog's URL format
+                changefreq: 'daily',
+                priority: 0.7,
+                lastmod: post.updatedat // Assuming you have a timestamp for when the post was last updated
+            });
+        });
 
-        // // End the stream
-        // sitemapStream.end();
+        // End the stream
+        sitemapStream.end();
 
-        // // Convert the stream to a string and send it as a response
-        // const sitemap = await streamToPromise(sitemapStream);
-        // res.header('Content-Type', 'application/xml');
-        // res.send(sitemap.toString());
+        // Convert the stream to a string and send it as a response
+        const sitemap = await streamToPromise(sitemapStream);
+        res.header('Content-Type', 'application/xml');
+        res.send(sitemap.toString());
     } catch (error) {
         console.error('Error generating sitemap:', error);
         res.status(500).send('Error generating sitemap');
